6c684fcb77efb7d82b7716e9137de32fe1e02b93
Kiril Solomatin <pa22yog@eduvaud.ch>
2025-08-29 15:58:06 +0200

    x

---
+++ secretcode_SolomatinKiril/secretcode_SolomatinKiril/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secretcode_SolomatinKiril
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Menu
+            string Square = "■" ;
+            
+            Console.WriteLine("╔══════════════════Kiril Solomatin══════════════════╗");
+            Console.WriteLine("║                                                   ║");
+            Console.WriteLine("║        Bienvenue dans le jeu: Secret Code         ║");
+            Console.WriteLine("║                                                   ║");
+            Console.WriteLine("╚═══════════════════════════════════════════════════╝");
+            Console.WriteLine();
+
+            Console.WriteLine("Un code secret composé de 4 chiffres est généré.\nÀ toi de le découvrir en 10 essais maximum !");
+            Console.WriteLine();
+
+            Console.WriteLine("À chaque essai, tu reçois un indice selon le niveau choisi.");
+            Console.WriteLine();
+
+            Console.WriteLine("Pour les niveaux 1 et 3 avec indices visibles :");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(Square + " ");
+            Console.ResetColor();
+            Console.WriteLine(": chiffre bien placé");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(Square + " ");
+            Console.ResetColor();
+            Console.WriteLine(": chiffre correct mais mal placé");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(Square + " ");
+            Console.ResetColor();
+            Console.WriteLine(": chiffre absent");
+            Console.WriteLine() ;
+
+            Console.WriteLine("Exemple :\nCode secret : 1234 (caché)\nVotre essai : 1325\nIndice");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(Square);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(Square);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(Square);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(Square);
+            Console.ResetColor();
+            Console.WriteLine(" (1 bien placé, 2 mal placés, 1 absent)");
+            Console.WriteLine();
+
+            Console.WriteLine("Pour les niveaux 2 et 4 avec indices discrets :\nExemple :\nCode secret : 5413 (caché)\nVotre essai : 1234\nIndice\t:");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("→ 0 bien placé(s), 3 mal placé(s)");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("Appuie sur une touche pour commencer...  ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ReadLine();
+        }
+            
+    }
+}
