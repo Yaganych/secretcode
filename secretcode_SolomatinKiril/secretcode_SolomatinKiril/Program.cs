c46ae73f40c16903ca700ab11338b02ede700a85
Yaganych <babayagakiril@gmail.com>
2025-08-31 18:40:24 +0200

    terminer le travail sur la sélection des niveaux, commencer le travail sur le premier niveau

--- secretcode_SolomatinKiril/secretcode_SolomatinKiril/Program.cs
+++ secretcode_SolomatinKiril/secretcode_SolomatinKiril/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,6 @@ namespace secretcode_SolomatinKiril
         {
             //Menu
             string Square = "■";
-            string levelNumber;
 
             Console.WriteLine("╔══════════════════Kiril Solomatin══════════════════╗");
             Console.WriteLine("║                                                   ║");
@@ -60,7 +60,7 @@ namespace secretcode_SolomatinKiril
 
             Console.WriteLine("Pour les niveaux 2 et 4 avec indices discrets :\nExemple :\nCode secret : 5413 (caché)\nVotre essai : 1234\nIndice\t:");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("→ 0 bien placé(s), 3 mal placé(s)");
+            Console.WriteLine("0 bien placé(s), 3 mal placé(s)");
             Console.ResetColor();
             Console.WriteLine();
 
@@ -70,6 +70,9 @@ namespace secretcode_SolomatinKiril
 
             Console.ReadKey();
             Console.Clear();
+            //level selection
+            bool valueOk = false;
+            int levelNumber = 0;
 
             Console.WriteLine("=== SECRET CODE ===");
             Console.WriteLine();
@@ -83,12 +86,99 @@ namespace secretcode_SolomatinKiril
 
             Console.WriteLine("Votre choix (1-4) :");
 
-            levelNumber = Console.ReadLine();
+            while (!valueOk || levelNumber < 1 || levelNumber > 4)
+            {
+                valueOk = int.TryParse(Console.ReadLine(), out levelNumber);
 
-            Console.WriteLine(levelNumber);
+                if (valueOk && levelNumber >= 1 && levelNumber <= 4)
+                {
+                    if ( levelNumber == 1 )
+                    {
+                        Debutant();
+                    }
 
+                    if (levelNumber == 2)
+                    {
+                        Intermediate();
+                    }
+
+                    if (levelNumber == 3)
+                    {
+                        Advanced();
+                    }
+
+                    if (levelNumber == 4)
+                    {
+                        Expert();
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Choix invalide. Essaie encore.");
+                }
+            }
+            
+            
+        }
+        static void Debutant()  
+        {
+            Console.Clear();
+            Console.WriteLine("=== SECRET CODE Niveau 1 ===");
+
+            Random random = new Random();
+            int hiddenNumber = random.Next(1234, 6544);
+            int userNumber;
+            bool validInput = false;
+            bool numberGuessed = false;
+
+            Console.WriteLine(hiddenNumber);
+
+            while (!numberGuessed)
+            {
+                Console.Write("Ente 4 chiffres entre 1 et 6 (ex: 1234) : ");
+
+                validInput = int.TryParse(Console.ReadLine(), out userNumber);
+
+                if (validInput && userNumber != hiddenNumber)
+                {
+                    Console.WriteLine(userNumber + " Malheureusement, ce n'est pas un numéro secret");
+                }
+                
+                else if (validInput && userNumber == hiddenNumber)
+                {
+                    Console.WriteLine("Félicitations, le nombre " + userNumber + " est un numéro secret");
+                    numberGuessed = true;
+                }
+
+                else if (!validInput)
+                {
+                    Console.WriteLine("Tu n'as pas entré un nombre! essaie de nouveau");
+                }
+
+            }
             Console.ReadLine();
         }
-            
+
+        static void Intermediate()
+        {
+            Console.Clear();
+            Console.WriteLine("=== SECRET CODE Niveau 2 ===");
+            Console.ReadLine();
+        }
+
+        static void Advanced()
+        {
+            Console.Clear();
+            Console.WriteLine("=== SECRET CODE Niveau 3 ===");
+            Console.ReadLine();
+        }
+
+        static void Expert()
+        {
+            Console.Clear();
+            Console.WriteLine("=== SECRET CODE Niveau 4 ===");
+            Console.ReadLine();
+        }
     }
 }
